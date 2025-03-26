a2ecdc6c5a64ab3120950e77bbabbd1f3bac6e13
ak4422920 <ak4422920@gmail.com>
2025-03-26 18:00:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,19 +5,19 @@ import logging
 
 #--------------------------------------------------------------------------------------------------------------------------#
 # Telegram API credentials
-API_ID = 12323533 # Replace with your API_ID 
-API_HASH = "" # Replace with your HASH
+API_ID = 29171167 # Replace with your API_ID 
+API_HASH = "7ea2149629e445936619f06a3c0dc716" # Replace with your HASH
 BOT_TOKEN = "" # Replace with your BOT TOKEN
 BOT_UN = "comick_dlbot" # Replace with your BOT Username Without @
 
 
 # MongoDB setup
-DB_URL = "" # Replace with your mongo db url
-DB_NAME = "Comick" # Do Need To Change This 
+DB_URL = "mongodb+srv://ak:ak@cluster0.snpjz8c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" # Replace with your mongo db url
+DB_NAME = "Cluster0" # Do Need To Change This 
 
 # Admin and Channel details
-ADMIN = 1234543523  # Replace with your Telegram user ID
-CHANNEL = "" # Replace with your force sub channel username. add bot as admin in yourforce sub channel before start the bot  
+ADMIN = 7251898668  # Replace with your Telegram user ID
+CHANNEL = "-1002442422204" # Replace with your force sub channel username. add bot as admin in yourforce sub channel before start the bot  
 DB_CHANNEL_UN = ""  # Replace with your File store channel username .Must Be Public
 IS_NOTIFY = "True"   # "True" or "False". If "True" Then It Will Send New Aired Chapter Notification In DM
 
